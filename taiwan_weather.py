92e570d38318dd84775a961142c4b67e9799d47e
KirillDmit <kirilldmitriev20015@gmail.com>
2022-06-21 19:04:34 +0500

    Update taiwan_weather.py

--- taiwan_weather.py
+++ taiwan_weather.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 cities = dict()
-cities["Taipei"] = "https://weather.com/en-GB/weather/today/l/fe7393b7f2c8eed2cf692bd079361df362d9f0c1c0f896e6e46a649295e15c7d"
+cities["Taipei"] = "https://weather.com/en-GB/weather/today/l" \
+                   "/fe7393b7f2c8eed2cf692bd079361df362d9f0c1c0f896e6e46a649295e15c7d "
 
 city = input()
 url = cities[city]
