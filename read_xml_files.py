6cdbe60aada4ad4c22cdede703f184a2471dde3f
KirillDmit <kirilldmitriev20015@gmail.com>
2022-06-21 19:41:36 +0500

    Update read_xml_files.py

--- read_xml_files.py
+++ read_xml_files.py
@@ -4,6 +4,11 @@ from bs4 import BeautifulSoup
 url = 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricText?lyricText=wonderwall'
 web = requests.get(url)
 soup = BeautifulSoup(web.content, 'xml')
-print(soup)
+# print(soup)
+print(soup.find("Artist").text)
+print(soup.find("Song").text)
+print(soup.find("SongRank").text)
+print(soup.find("SearchLyricResult").find("ArtistUrl").text)
+print(len(soup.findAll("SearchLyricResult")))
 
 
