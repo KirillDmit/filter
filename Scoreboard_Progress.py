3c0963221f70184ad5e0b624822c3554fa0e96f8
KirillDmit <kirilldmitriev20015@gmail.com>
2022-06-23 00:13:56 +0500

    Update Scoreboard_Progress.py

--- Scoreboard_Progress.py
+++ Scoreboard_Progress.py
@@ -9,11 +9,26 @@ else:
 
 while True:
     try:
-        game_score = int(input("Please choose the game, enter 301 or 501: "))
+        game_score = int(input("Choose the game, enter 301 or 501: "))
         if game_score in [301, 501]:
             print("OK!")
             break
         else:
             print("Incorrect number for the game score")
+    except ValueError:
+        print("Enter a number for the game score, either 301 or 501")
+
+player_avg = []
+while game_score > 0:
+    player_score = int(input("Enter player score:"))
+    player_avg.append(player_score)
+    game_score -= player_score
+    print(str(game_score) + " remaining")
+
+if game_score <= 0:
+    avg = round(mean(player_avg))
+    print(f"Average for this game was {avg}")
+
+
 
 
